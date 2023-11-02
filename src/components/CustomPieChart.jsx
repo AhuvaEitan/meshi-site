0fd11b54e66d3cbb95b4a57596f486f42c256a0a
AhuviEitan <a0527638406@gmail.com>
2023-11-02 20:49:00 +0200

    #it works!!

--- src/components/CustomPieChart.jsx
+++ src/components/CustomPieChart.jsx
@@ -1,6 +1,7 @@
-
 import React, { useState } from "react";
 import { PieChart, Pie, Cell, Sector } from "recharts";
+import Succeeded from "./Succeeded";
+
 
 const initialData = [
   { name: "Part 1", value: 25 },
@@ -8,153 +9,162 @@ const initialData = [
   { name: "Part 3", value: 25 },
   { name: "Part 4", value: 25 },
 ];
+      
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#AF19FF",
+  "#FF19AF",
+];
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
+const COLORSDATA=[
+  "red",
+  "blue",
+  "pink",
+  "#AF19FF"
+]
 const CustomPieChart = () => {
   const [data, setData] = useState(initialData);
   const [activeIndex, setActiveIndex] = useState(null);
+ const[part,setPart]=useState(null);
 
-  // const onPieClick = (clickedData, index) =>
-  //  {
-  //   if (activeIndex === index) 
-  //   {
-  //     setActiveIndex(null);
+  
 
-  //   //new
-  //     const newData = [...data];
-  //     // Reset the clicked part to its original value
-  //     newData[index].value = initialData[index].value;
-  //     setData(newData);
-  //   } 
-  //   else {
-  //     const newData = [...data];
-  //     const newValue = newData[index].value / 4;
-
-  //     newData[index].value = newValue;
-
-  //     for (let i = 1; i <= 3; i++) {
-  //       newData.splice(index + i, 0, { name: `Part ${index + i + 1}`, value: newValue });
-  //     }
-
-  //     setActiveIndex(index);
-  //     setData(newData);
-     
-  //   }
   // const onPieClick = (clickedData, index) => {
-  //   if (activeIndex === index) {
-  //     setActiveIndex(null);
-  //   } else {
-  //     const newData = data.map((item, i) => {
-  //       if (i === index) {
-  //         const newValue = item.value / 4;
-  //         return { name: `Part ${i + 1}`, value: newValue };
-  //       } else {
-  //         return item;
-  //       }
-  //     });
-  
-  //     const dividedParts = [];
-  //     for (let i = 0; i < 3; i++) {
-  //       dividedParts.push({ name: `Part ${index + i + 2}`, value: newData[index].value });
-  //     }
-  
-  //     newData.splice(index + 1, 0, ...dividedParts);
-  //     setActiveIndex(index);
-  //     setData(newData);
-  //   }
-  // };
-
-  // const onPieClick = (clickedData, index) => 
-  // {
+  //   console.log("ind",index)
   //   if (activeIndex === index) {
   //     setActiveIndex(null);
   //   } else {
   //     const originalValue = data[index].value;
   //     const newValue = originalValue / 4;
-  
+ 
+
   //     const newData = data.map((item, i) => {
-  //       if (i === index) {
-  //         return { ...item, value: originalValue - (3 * newValue) };
-  //       } else {
+  //       // debugger;
+        
+  //       // if (i === index) {
+
   //         return { ...item, value: newValue };
-  //       }
+  //       // } else if (item.value !== newValue) {
+  //       //   return { ...item, value: newValue };
+  //       // }
+  //       return item;
   //     });
-  
+  //     console.log(newData)
   //     setActiveIndex(index);
   //     setData(newData);
   //   }
   // };
 
   const onPieClick = (clickedData, index) => {
-    if (activeIndex === index) {
-      setActiveIndex(null);
-    } else {
-      const originalValue = data[index].value;
-      const newValue = originalValue / 4;
+    // Update the clicked quadrants
+    // setClickedQuadrants(prevQuadrants => [...prevQuadrants, index]);
+    
+    // Divide the clicked part into four
+    const originalValue = data[index].value;
+    const newValue = originalValue / 4;
   
+    // const newData = data.map((item, i) => {
+    //   // if (i === index) {
+    //     return { ...item, value: newValue };
+    //   // }
+    //   return item;
+    // });
+
       const newData = data.map((item, i) => {
         if (i === index) {
           return { ...item, value: originalValue - (3 * newValue) };
         } else if (item.value !== newValue) {
-          return { ...item, value: newValue };
+          return { ...item, value: newValue };  
         }
         return item;
       });
-  
-      setActiveIndex(index);
-      setData(newData);
-    }
+    
+      setActiveIndex(index); 
+    // Update the data with the new values
+    setData(newData);
   };
+  
   
     
   // };
 
 
   return (
+
+    <div>
+
     <PieChart width={4000} height={4000}>
       <Pie
         activeIndex={activeIndex}
         activeShape={({ cx, cy, innerRadius, outerRadius, startAngle, endAngle }) => (
           <g>
             <text x={cx} y={cy} dy={8} textAnchor="middle" fill={COLORS[activeIndex]}>
-              
+          
             </text>
             <Sector
               cx={cx}
+              
               cy={cy}
               innerRadius={innerRadius}
               outerRadius={outerRadius}
               startAngle={startAngle}
-              endAngle={(endAngle - startAngle) / 4 + startAngle}
-              fill={COLORS[activeIndex]}
+              endAngle={(endAngle - startAngle) / 6 + startAngle}
+              fill={COLORS[activeIndex]} 
+              onClick={()=>{setPart(0)}}
+              
             />
             <Sector
               cx={cx}
               cy={cy}
               innerRadius={innerRadius}
               outerRadius={outerRadius}
-              startAngle={(endAngle - startAngle) / 4 + startAngle}
-              endAngle={(endAngle - startAngle) / 2 + startAngle}
+              startAngle={(endAngle - startAngle) / 6 + startAngle}
+              endAngle={(endAngle - startAngle) / 3 + startAngle}
               fill={COLORS[(activeIndex + 1) % COLORS.length]}
+              onClick={()=>{setPart(1)}}
             />
             <Sector
               cx={cx}
               cy={cy}
               innerRadius={innerRadius}
               outerRadius={outerRadius}
-              startAngle={(endAngle - startAngle) / 2 + startAngle}
-              endAngle={(endAngle - startAngle) * 3 / 4 + startAngle}
+              startAngle={(endAngle - startAngle) / 3 + startAngle}
+              endAngle={(endAngle - startAngle) /2 + startAngle}
               fill={COLORS[(activeIndex + 2) % COLORS.length]}
+              onClick={()=>{setPart(2)}}
             />
             <Sector
               cx={cx}
               cy={cy}
               innerRadius={innerRadius}
               outerRadius={outerRadius}
-              startAngle={(endAngle - startAngle) * 3 / 4 + startAngle}
-              endAngle={endAngle}
+              startAngle={(endAngle - startAngle) /2 + startAngle}
+              endAngle={(endAngle - startAngle) *2/3 + startAngle}
               fill={COLORS[(activeIndex + 3) % COLORS.length]}
+              onClick={()=>{setPart(3)}}
+            />
+            <Sector
+              cx={cx}
+              cy={cy}
+              innerRadius={innerRadius}
+              outerRadius={outerRadius}
+              startAngle={(endAngle - startAngle) *2/3 + startAngle}
+              endAngle={(endAngle - startAngle) *5/6 + startAngle}
+              fill={COLORS[(activeIndex + 4) % COLORS.length]}
+              onClick={()=>{setPart(4)}}
+            />
+            <Sector
+              cx={cx}
+              cy={cy}
+              innerRadius={innerRadius}
+              outerRadius={outerRadius}
+              startAngle={(endAngle - startAngle)  *5/6  + startAngle}
+              endAngle={endAngle}
+              fill={COLORS[(activeIndex + 5) % COLORS.length]}
+              onClick={()=>{setPart(5)}}
             />
           </g>
         )}
@@ -167,10 +177,19 @@ const CustomPieChart = () => {
         onClick={(clickedData, index) => onPieClick(clickedData, index)}
       >
         {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          <Cell key={`cell-${index}`} fill={COLORSDATA[index % COLORSDATA.length] } />
         ))}
       </Pie>
     </PieChart>
+   {console.log("part=",part)}
+    {part!==null && <Succeeded part={part}></Succeeded>}
+    {/* {part===1 && <Succeeded part={2}/>}
+    {part===2 && <Succeeded part={3}/>}
+    {part===3 && <Succeeded part={4}/>}
+    {part===4 && <Succeeded part={5}/>}
+    {part===5 && <Succeeded part={6}/>} */}
+    
+    </div>
   );
 };
 
---
+++ src/components/Succeeded.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Succeeded({part}){
+    return(
+       <h1>SUCCEED! {part}</h1>
+    );
+}
+
+export default Succeeded;
